77e07493bd2dc081b112475d15b5e6bf1d4529e5
lmrick <raphaallemos@gmail.com>
2026-07-21 18:19:14 -0300

    fix: replace NotNull with NotBlank in auth request validation, add @Schema

--- src/main/java/com/lmrick/timescheduler/infrastructure/dto/AuthRequestDTO.java
+++ src/main/java/com/lmrick/timescheduler/infrastructure/dto/AuthRequestDTO.java
@@ -1,10 +1,21 @@
 package com.lmrick.timescheduler.infrastructure.dto;
 
-import jakarta.validation.constraints.NotNull;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 
 public record AuthRequestDTO(
-				@NotNull String username,
-				@NotNull String password
+				@Schema(
+								description = "Username used for authentication",
+								example = "john.doe"
+				)
+				@NotBlank String username,
+				
+				@Schema(
+								description = "User password",
+								example = "********",
+								format = "password"
+				)
+				@NotBlank String password
 ) {
 
 }
