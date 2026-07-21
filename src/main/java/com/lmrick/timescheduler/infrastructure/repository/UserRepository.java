2e4f35f2c4352ab5b0155115b5e575b37eb0511c
lmrick <raphaallemos@gmail.com>
2026-07-21 16:41:28 -0300

    Add @Repository annotation to UserRepository

--- src/main/java/com/lmrick/timescheduler/infrastructure/repository/UserRepository.java
+++ src/main/java/com/lmrick/timescheduler/infrastructure/repository/UserRepository.java
@@ -2,8 +2,10 @@ package com.lmrick.timescheduler.infrastructure.repository;
 
 import com.lmrick.timescheduler.infrastructure.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 	
 	Optional<UserEntity> findByUsername(String username);
